085008ac6ce12c1c4af1fce44623bb6141f180dc
JSeater <111250201+JSeater@users.noreply.github.com>
2023-11-28 15:04:37 -0800

    nov 28 (2)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ComputerVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ComputerVision.java
@@ -1,10 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.core.Core;
 import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
@@ -15,50 +15,23 @@ import org.openftc.easyopencv.OpenCvPipeline;
 public class ComputerVision {
 
     private OpenCvCamera phoneCam;
-
-    // change matrix region to whatever is best visible
-    // width of image is 640
-    // height of image is 480
-    private final int[][] region =  {{280 - 50, 480 - 0}, {280 + 50, 480 + 40}};//new int[2][2];
-    /*
-    region[0][0] = 320 - 70;    // top left x
-    region[0][1] = 240 - 20;    // top left y
-    region[1][0] = 320 + 70;    // bottom right x
-    region[1][1] = 240 + 20;    // bottom right y
-    */
-    // change these colors to whatever you select. make sure you choose appropriate min and max thresholds
-    private final int[][] magenta = {{80, 50, 82}, {120, 90, 122}};
-    /*
-    magenta[0][0] = 100; // color1 min R
-    magenta[0][1] = 0;   // color1 min G
-    magenta[0][2] = 100; // color1 min B
-    magenta[1][0] = 255; // color1 max R
-    magenta[1][1] = 100; // color1 min G
-    magenta[1][2] = 255; // color1 max B
-    */
-    private final int[][] orange = {{110, 89, 80}, {130, 129, 120}};
-
-    /*
-    orange[0][0] = 255;   // color2 min R
-    orange[0][1] = 100; // color2 min G
-    orange[0][2] = 0;   // color2 min B
-    orange[1][0] = 255; // color2 max R
-    orange[1][1] = 150; // color2 max G
-    orange[1][2] = 30; // color2 max B
-    */
-    private final int[][] green = {{55, 83, 73}, {95, 123, 113}};
-    /*
-    green[0][0] = 0; // color3 min R
-    green[0][1] = 100;  // color3 min G
-    green[0][2] = 0;   // color3 min B
-    green[1][0] = 50; // color3 max R
-    green[1][1] = 255; // color3 max G
-    green[1][2] = 20; // color3 max B
-    */
-    private int[] avgRGB = new int[3];
-    private int recognition=4;
+    private final int fractions = 12;
+    private final double[][] topLeft = new double[fractions][2];
+    private final double[][] botRight = new double[fractions][2];
+    private int[][] avgRGB = new int[fractions][3];
+    private final int[] RED = {0, 200};
+    private final int[] GREEN = {50, 255};
+    private final int[] BLUE = {0, 200};
+    private int x;
+    private int[] longestSeq = new int[2];
 
     public ComputerVision(int camId) {
+        for (int i = 0; i < fractions; i++) {
+            topLeft[i][0] = 0d / 3d;
+            topLeft[i][1] = ((double) i) / ((double) fractions);
+            botRight[i][0] = 1d / 3d;
+            botRight[i][1] = ((double) (i + 1)) / ((double) fractions);
+        }
         phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, camId);
         phoneCam.setPipeline(new Pipeline());
         phoneCam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
@@ -72,39 +45,76 @@ public class ComputerVision {
         });
     }
 
-    public boolean isColor(int[][] color) {
-        return (
-                color[0][0] <= avgRGB[0] && avgRGB[0] <= color[1][0] &&
-                        color[0][1] <= avgRGB[1] && avgRGB[1] <= color[1][1] &&
-                        color[0][2] <= avgRGB[2] && avgRGB[2] <= color[1][2]
-        );
-    }
+    public int getX() {return x;}
 
-    public void analyze() {
-        if (isColor(magenta)) {
-            recognition = 2;
-        } else if (isColor(orange)) {
-            recognition = 3;
-        } else if (isColor(green)){
-            recognition = 1;
-        } else
-            recognition = 4;
+    public int[] getAnalysis() {
+        /*int output = -1;
+        int max_red = 0;
+        for (int i = 0; i < fractions; i++) {
+            if (avgRGB[i][1] < MAX_GREEN * avgRGB[i][0] && avgRGB[i][1] > MIN_GREEN * avgRGB[i][0] && avgRGB[i][2] < MAX_BLUE * avgRGB[i][0] && (output == -1 || avgRGB[output][0] <= avgRGB[i][0])) {
+                output = i;
+            }
+            if (avgRGB[i][0] > max_red) {
+                output = i;
+                max_red = avgRGB[i][0];
+            }
+        }
+        return output;
+        */
+        x+=1;
+        longestSeq = new int[2];
+        boolean[] seq = new boolean[fractions];
+        for (int i = 0; i < fractions; i++) {
+            seq[i] = (
+                    (RED[0] < avgRGB[i][0] && avgRGB[i][0] < RED[1]) &&
+                            (GREEN[0] < avgRGB[i][1] && avgRGB[i][1] < GREEN[1]) &&
+                            (BLUE[0] < avgRGB[i][2] && avgRGB[i][2] < BLUE[1]) &&
+                            (avgRGB[i][1] * 0.9 > avgRGB[i][0] && avgRGB[i][1] * 0.9 > avgRGB[i][2])
+            );
+        }
+        int[] currentSeq = new int[2];
+        boolean prevTrue = false;
+        for (int i = 0; i < fractions; i++) {
+            if (seq[i]) {
+                if (prevTrue) {
+                    currentSeq[1] = i;
+                } else {
+                    currentSeq[0] = i;
+                }
+                prevTrue = true;
+            } else {
+                if (prevTrue) {
+                    prevTrue = false;
+                    currentSeq[1] = i;
+                }
+                if (currentSeq[1] - currentSeq[0] > longestSeq[1] - longestSeq[0]) {
+                    longestSeq[0] = currentSeq[0];
+                    longestSeq[1] = currentSeq[1];
+                }
+            }
+        }
+        if (prevTrue) {
+            currentSeq[1] = fractions;
+        }
+        if (currentSeq[1] - currentSeq[0] > longestSeq[1] - longestSeq[0]) {
+            longestSeq[0] = currentSeq[0];
+            longestSeq[1] = currentSeq[1];
+        }
+        return longestSeq;
     }
 
     public int getRecognition() {
-        return recognition;
+        return (int) (Math.round((double) (longestSeq[1] + longestSeq[0]) / 2.)) / (fractions / 3);
     }
 
-    public int[] getRGB() {
-        return avgRGB;
-    }
+    public int[][] getRGB() {return avgRGB;}
 
     class Pipeline extends OpenCvPipeline {
 
         private boolean viewportPaused = false;
         private Mat YCrCb = new Mat();
         private Mat Cb = new Mat();
-        private Mat regionMatrix;
+        private Mat[] regions = new Mat[fractions];
 
         private void inputToCb(Mat input) {
             Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
@@ -114,31 +124,43 @@ public class ComputerVision {
         @Override
         public void init(Mat firstFrame) {
             inputToCb(firstFrame);
-            regionMatrix = firstFrame.submat(new Rect(
-                    new Point(region[0][0], region[0][1]),
-                    new Point(region[1][0], region[1][1])
-            ));
+            for (int i = 0; i < fractions; i++) {
+                regions[i] = firstFrame.submat(new Rect(
+                        new Point(firstFrame.cols() * topLeft[i][0], firstFrame.rows() * topLeft[i][1]),
+                        new Point(firstFrame.cols() * botRight[i][0], firstFrame.rows() * botRight[i][1])
+                ));
+            }
+
         }
 
         @Override
         public Mat processFrame(Mat input) {
-            regionMatrix = input.submat(new Rect(
-                    new Point(region[0][0], region[0][1]),
-                    new Point(region[1][0], region[1][1])
-            ));
-            double[] avg;
-            avg = Core.mean(regionMatrix).val;
-            for (int i = 0; i < 3; i++) {
-                avgRGB[i] = (int) avg[i];
+            for (int i = 0; i < fractions; i++) {
+                regions[i] = input.submat(new Rect(
+                        new Point(input.cols() * topLeft[i][0], input.rows() * topLeft[i][1]),
+                        new Point(input.cols() * botRight[i][0], input.rows() * botRight[i][1])
+                ));
             }
-            Imgproc.rectangle(
-                    input,
-                    new Point(region[0][0], region[0][1]),
-                    new Point(region[1][0], region[1][1]),
-                    new Scalar(0, 0, 255),
-                    4
-            );
-            analyze();
+            double[] avg;
+            for (int i = 0; i < fractions; i++) {
+                avg = Core.mean(regions[i]).val;
+                for (int j = 0; j < 3; j++) {
+                    avgRGB[i][j] = (int) avg[j];
+                }
+            }
+            getAnalysis();
+            if (longestSeq[0] != longestSeq[1]) {
+                Imgproc.rectangle(
+                        input,
+                        new Point(
+                                input.cols() * topLeft[longestSeq[0]][0],
+                                input.rows() * topLeft[longestSeq[0]][1]),
+                        new Point(
+                                input.cols() * botRight[longestSeq[1] - 1][0],
+                                input.rows() * botRight[longestSeq[1] - 1][1]),
+                        new Scalar(0, 255, 0), 4);
+            }
+
             return input;
         }
 
