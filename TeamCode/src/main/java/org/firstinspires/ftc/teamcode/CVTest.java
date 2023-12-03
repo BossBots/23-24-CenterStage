1b0cabf2ae402b809ec6781bf6be559f8d96de9a
aizilerts <annacpineapple2017@gmail.com>
2023-12-02 18:50:13 -0800

    dec 2 23

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CVTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CVTest.java
@@ -1,40 +1,24 @@
-//package org.firstinspires.ftc.teamcode;
-//
-//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//
-///*
-//This program is for testing ONLY. It is TeleOp.
-//This program is for testing ComputerVision. It prints avgRGB within the targetted region and recognition to the screen.
-//*/
-//
-//
-//@TeleOp
-//public class CVTest extends LinearOpMode {
-//
-//    private ComputerVision cv;
-//    private int recognition;
-//    private int[][] avgRGB;
-//
-//    @Override
-//    public void runOpMode() throws InterruptedException {
-//
-//        cv = new ComputerVision(hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName()));
-//
-//        waitForStart();
-//
-//        if (opModeIsActive()) {
-//            while (opModeIsActive()) {
-//
-//                recognition = cv.getRecognition();   // recognition detected by ComputerVision
-//                avgRGB = cv.getRGB();   // getting avg RGB values in the region
-//
-//                telemetry.addData("recognition", recognition);
-//                telemetry.addData("avg R", avgRGB[0]);
-//                telemetry.addData("avg G", avgRGB[1]);
-//                telemetry.addData("avg B", avgRGB[2]);
-//                telemetry.update();
-//            }
-//        }
-//    }
-//}
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class CVTest extends LinearOpMode {
+
+    private ComputerVision cv;
+    private int[][] rgb;
+
+    @Override
+    public void runOpMode() {
+        cv = new ComputerVision(hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName()));
+        waitForStart();
+
+        while (opModeIsActive()) {
+            rgb = cv.getRGB();
+            telemetry.addData("index", cv.getRecognition());
+            telemetry.addData("x", cv.getX());
+            telemetry.update();
+        }
+    }
+}
