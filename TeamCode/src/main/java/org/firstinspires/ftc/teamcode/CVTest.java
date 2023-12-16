6379d1f3320a08854d67e1e50f3afe2dd5fa7b9e
aizilerts <annacpineapple2017@gmail.com>
2023-12-16 09:39:06 -0800

    12/16/23

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CVTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CVTest.java
@@ -9,6 +9,7 @@ public class CVTest extends LinearOpMode {
     private ComputerVision cv;
     private int[][] rgb;
 
+    private int middleRGBAverage;
     @Override
     public void runOpMode() {
         cv = new ComputerVision(hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName()));
@@ -17,6 +18,19 @@ public class CVTest extends LinearOpMode {
         while (opModeIsActive()) {
             rgb = cv.getRGB();
             telemetry.addData("index", cv.getRecognition());
+            int[][] rgb = cv.getRGB();
+            double[] avg = new double[3];
+            for (int i = 4; i < 8; i++) {
+                avg[0] += rgb[i][0];
+                avg[1] += rgb[i][1];
+                avg[2] += rgb[i][2];
+            }
+            avg[0] /= 4;
+            avg[1] /= 4;
+            avg[2] /= 4;
+            telemetry.addData("red", avg[0]);
+            telemetry.addData("green", avg[1]);
+            telemetry.addData("blue", avg[2]);
             telemetry.addData("x", cv.getX());
             telemetry.update();
         }
